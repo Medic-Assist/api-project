40f5cf752f68598138e46a53edf97c3cb32a0a5c
Alizee <alizeehett@hotmail.fr>
2024-10-02 16:38:48 +0200

    ADD init sql

---
+++ db/init.sql
@@ -0,0 +1,11 @@
+-- db/init.sql
+
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(100),
+  email VARCHAR(100)
+);
+
+INSERT INTO users (name, email) VALUES
+('John Doe', 'john@example.com'),
+('Jane Smith', 'jane@example.com');
