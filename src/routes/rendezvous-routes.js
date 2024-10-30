d8caf873df8212613a0a2ce32a77a337a6c3ebf5
charlineBx <charline.beaudeux8@etu.univ-lorraine.fr>
2024-10-30 09:16:47 +0100

    requetes pour les RDV

--- src/routes/rendezvous-routes.js
+++ src/routes/rendezvous-routes.js
@@ -2,37 +2,37 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../db");
 
-// Obtenir tous les utilisateurs
+// Obtenir tous les rendez-vous
 router.get("/", async (req, res) => {
   try {
     const result = await pool.query("SELECT * FROM RDV");
     res.json(result.rows);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send("Server error");
+    res.status(500).send("Server error");gi
   }
 });
 
-// Ajouter un utilisateur
+// Ajouter un rendez-vous
 router.post("/", async (req, res) => {
   try {
-    const { name, email } = req.body;
-    const newUser = await pool.query(
-      "INSERT INTO Utilisateur (name, email) VALUES ($1, $2) RETURNING *",
-      [name, email]
+    const { intitule,horaire, dateRDV, idUser, idCentreMed, isADRPrincipale } = req.body;
+    const newRDV = await pool.query(
+      "INSERT INTO RDV (intitule,horaire, dateRDV, idUser, idCentreMed, isADRPrincipale) VALUES ($1, $2,$3,$4,$5,$6) RETURNING *",
+      [intitule,horaire, dateRDV, idUser, idCentreMed, isADRPrincipale]
     );
-    res.json(newUser.rows[0]);
+    res.json(newRDV.rows[0]);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
   }
 });
 
-// Obtenir un utilisateur par ID
+// Obtenir la liste de rendez-vous poiur le patient avec l'ID donné
 router.get("/patient/:idUser", async (req, res) => {
   try {
     const { idUser } = req.params;
-    const rdv = await pool.query("SELECT * FROM RDV WHERE idUser = $1", [idUser]);
+    const rdv = await pool.query("SELECT * FROM RDV LEFT JOIN CentreMedical CM ON CM.idCentreMed = RDV.idCentreMed WHERE idUser = $1", [idUser]);
    
     if (rdv.rows.length === 0) {
       return res.status(404).send("Aucun Rendez-Vous");
@@ -44,29 +44,44 @@ router.get("/patient/:idUser", async (req, res) => {
   }
 });
 
-// Mettre à jour un utilisateur
-router.put("/:id", async (req, res) => {
+// Mettre à jour l'horaire et la date d'un rendez-vous
+router.put("/:idUser/:idRDV", async (req, res) => {
   try {
-    const { id } = req.params;
-    const { name, email } = req.body;
-    await pool.query("UPDATE Utilisateur SET name = $1, email = $2 WHERE id = $3", [
-      name,
-      email,
-      id,
+    const { idUser,idRDV } = req.params;
+    const { horaire, dateRDV } = req.body;
+    await pool.query("UPDATE RDV SET horaire = $1, dateRDV = $2 WHERE idUser = $3 AND idRDV = 1", [
+      horaire,
+      dateRDV,
+      idUser,
+      idRDV
     ]);
-    res.send("User updated");
+    res.send("Rendez-vous modifié.");
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
   }
 });
 
-// Supprimer un utilisateur
-router.delete("/:id", async (req, res) => {
+// Mettre à jour l'info de quelle adresse regardé pour un rendez-vous
+router.put("/:idUser", async (req, res) => {
   try {
-    const { id } = req.params;
-    await pool.query("DELETE FROM Utilisateur WHERE id = $1", [id]);
-    res.send("User deleted");
+    const { idUser } = req.params;
+    await pool.query("UPDATE RDV SET isADRPrincipal = FALSE WHERE dateRDV = CURDATE() AND idUser = $1", 
+      [idUser]
+    );
+    res.send("Rendez-vous modifié.");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
+// Supprimer un rendez-vous
+router.delete("/:idRDV", async (req, res) => {
+  try {
+    const { idRDV } = req.params;
+    await pool.query("DELETE FROM RDV WHERE idRDV = $1", [idRDV]);
+    res.send("Rendez-vous supprimé.");
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server error");
